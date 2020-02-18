26023af53bce3caa84cd89ce15fee6ee9bdb3f88
JedlikFK <farkaskrisz9898@gmail.com>
2020-02-18 13:30:00 +0100

    közdés

--- billiard/Program.cs
+++ billiard/Program.cs
@@ -10,8 +10,21 @@ namespace billiard
     {
         static void Main(string[] args)
         {
-
+            List<int> golyok = new List<int>();
+            kezdes(golyok);
+            foreach (var item in golyok)
+            {
+                Console.WriteLine(item);
+            }
             Console.ReadKey();
+        }
+
+        static void kezdes(List<int> golyok)
+        {
+            for (int i = 1; i <= 15; i++)
+            {
+                golyok.Add(i);
+            }
         }
     }
 }
